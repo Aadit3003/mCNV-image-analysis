058f27e6ed76a12821f297173450d7db91036049
Aadit3003 <82210227+Aadit3003@users.noreply.github.com>
2021-10-03 09:08:54 +0530

    vessel diameter

--- derived_parameters.py
+++ derived_parameters.py
@@ -4,7 +4,8 @@ import pandas as pd
 measurements = pd.read_csv("Measurements/Measurements_Tort_fixed.csv")
 measurements["Vessel Density"] = measurements["Vessel Area"]/measurements["mCNV Area"]
 measurements["Junction Density"] = measurements["Vessel Junctions"]/measurements["Vessel Length"]
-measurements.to_csv("Measurements/Derived_Tort_fixed.csv", index=False)
+measurements["Vessel Diameter"] = 1000*measurements["Vessel Area"]/measurements["Vessel Length"]
+measurements.to_csv("Measurements/Derived_Complete.csv", index=False)
 
 print("Done!")
 
